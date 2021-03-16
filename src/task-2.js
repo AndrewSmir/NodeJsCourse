a8be5aa111b3cfe7d73e7e798236de29c1711a19
Andrey <smirnov.andrey.n@gmail.com>
2021-03-16 20:34:33 +0300

    chore: replace require with import

--- src/task-2.js
+++ src/task-2.js
@@ -1,6 +1,6 @@
-const csv = require('csvtojson');
-const path = require('path');
-const fs = require('fs');
+import csv from 'csvtojson';
+import path from 'path';
+import fs from 'fs';
 
 const readStream = fs.createReadStream(path.join(__dirname, './csv/nodejs-hw1-ex1.csv'));
 const writeStream = fs.createWriteStream(path.join(__dirname, './csv/nodejs-hw1-ex2.txt'));
