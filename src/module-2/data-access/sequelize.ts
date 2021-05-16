b4e60a1969826af6c1a1d97928769291c0716e93
Andrey <sminrov.andrey.n@gmail.com>
2021-05-16 22:21:59 +0300

    feat: add winston logging

--- src/module-2/data-access/sequelize.ts
+++ src/module-2/data-access/sequelize.ts
@@ -2,7 +2,6 @@ import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize(
   'postgres://lhncmhvm:X39LCEp98OeKVd_DqMPcCp3VwnBhFydl@hattie.db.elephantsql.com:5432/lhncmhvm',
-  { logging: console.log },
 );
 
 sequelize.sync({ alter: true }).then(() => console.log('ok'));
--- src/module-2/decorators/try-catch.ts
+++ src/module-2/decorators/try-catch.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from 'express';
+import { logger } from '../loggers';
 
 export function TryCatch(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
   const oldValue = descriptor.value;
   descriptor.value = async function (req: Request, res: Response) {
     try {
+      logger.info({ methodName: req.method, arguments: req.body });
       return await oldValue.call(this, req, res);
     } catch (e) {
-      console.error(e);
-      res.status(500).json({ error: e.message });
+      process.on('unhandledRejection', function (err) {
+        logger.error(`Unhandled rejection ${err}`);
+        res.status(500).json({ error: err });
+      });
+      await Promise.reject(e.message);
     }
   };
 }
